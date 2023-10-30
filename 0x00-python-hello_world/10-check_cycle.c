917875b920f89803294bae97cb56481d36aef14e
ahmed <ahmedarafa.200444@gmail.com>
2023-10-30 02:43:08 -0700

    .

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check_cycle
+ * @list: head
+ * Return: 1 if true 0 if false
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *test = list;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	while (test != NULL)
+	{
+		test = test.next;
+
+		if (test == head)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
