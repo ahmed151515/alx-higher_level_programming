03ff58c84012d717d7737eb29809e28d25c9966f
ahmed <ahmedarafa.200444@gmail.com>
2023-11-02 11:19:52 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    print("{} argument:".format(len(argv)))
-    for i in range(1, len(argv)):
-        print("{} {}".format(i, argv[i]))
+    if len(argv) == 1:
+        print("0 argument.")
+    else:
+        print("{} argument:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
