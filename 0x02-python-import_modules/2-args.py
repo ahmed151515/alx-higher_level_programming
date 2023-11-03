7c6263316db21f2b4fb20db42c9a4ee1540de8a6
ahmed <ahmedarafa.200444@gmail.com>
2023-11-03 04:12:39 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
-        print("1 {}".format(argv[1]))
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
