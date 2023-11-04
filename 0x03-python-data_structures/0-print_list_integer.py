14002f939c8ea480c694b03a8926058d0b20d23f
ahmed <ahmedarafa.200444@gmail.com>
2023-11-04 00:20:55 -0700

    .

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
-    exit(0)
-
-
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    def print_list_integer(my_list=[]):
+        for i in my_list:
+            print("{:d}".format(i))
