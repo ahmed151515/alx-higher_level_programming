882a30ebaee9704115393c80ffd82204a75829ea
ahmed <ahmedarafa.200444@gmail.com>
2023-11-08 08:51:49 -0800

    .

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,6 @@ def square_matrix_simple(matrix=[]):
     res = [[], [], []]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            res[i].append(matrix[i][j]**2)
+            if type(matrix[i][j]) == int:
+                res[i].append(matrix[i][j]**2)
     return res
