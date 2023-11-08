30e7a4dfaf21f1363fb7daf04deb0f460928ae7c
ahmed <ahmedarafa.200444@gmail.com>
2023-11-08 09:00:17 -0800

    .

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    tmp = [] # Recurring
+    tmp = []  # Recurring
     for i in my_list:
         if i in tmp:
             continue
         tmp.append(i)
         sum += i
     return sum
-
