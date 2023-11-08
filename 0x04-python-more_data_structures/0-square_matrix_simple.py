0d3ac4a23e8e67a9d1647226144b299c9d11d9cb
ahmed <ahmedarafa.200444@gmail.com>
2023-11-08 08:35:24 -0800

    .

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        return []
     res = [matrix[0].copy(), matrix[1].copy(), matrix[2].copy()]
     for i in range(len(res)):
         for j in range(len(res[i])):
