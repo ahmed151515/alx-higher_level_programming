a02457bc84068e5232ee12b34ae3dac1d9e6c184
ahmed <ahmedarafa.200444@gmail.com>
2023-11-20 06:22:41 -0800

    .

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
+    c = 0
 
     while (i < x):
         try:
             print("{:d}".format(my_list[i]), end='')
             i += 1
+            c += 1
         except (ValueError, TypeError):
             i += 1
             pass
 
     print()
-    return i
+    return c
