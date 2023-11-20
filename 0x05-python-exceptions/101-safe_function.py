76057efd0b927e2b644dcb06ec1e81f9feae6ac8
ahmed <ahmedarafa.200444@gmail.com>
2023-11-20 07:46:43 -0800

    .

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as ex:
+        print(f"Exception: {ex}", file=sys.stderr)
+        return None
