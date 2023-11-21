bb46fcd69322fe2d8ab66b471cc43a1a831aff9c
ahmed <ahmedarafa.200444@gmail.com>
2023-11-21 09:15:50 -0800

    .

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""0. My first square solve"""
+
+
+class Square:
+    """0. My first square solve"""
+
+    def __init__(self, size=0):
+        """__init__ method"""
+        self.size(size)
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        """setter method"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """get area"""
+        return self.__size ** 2
