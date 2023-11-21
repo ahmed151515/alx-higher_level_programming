f630156d39d475032faca0d0c6ba591309469941
ahmed <ahmedarafa.200444@gmail.com>
2023-11-21 09:39:43 -0800

    .

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,9 +5,10 @@
 class Square:
     """0. My first square solve"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """__init__ method"""
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -22,6 +23,17 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter method"""
+        if type(value) != tuple and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = value
+
     def area(self):
         """get area"""
         return self.__size ** 2
@@ -31,4 +43,5 @@ class Square:
             print
             return
         for i in range(self.size):
+            print(" " * self.position[0], end="")
             print("#" * self.size)
