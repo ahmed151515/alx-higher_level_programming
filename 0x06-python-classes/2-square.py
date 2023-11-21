74569aaf3c7aa9e4fee985f12ec59aea33e4e343
ahmed <ahmedarafa.200444@gmail.com>
2023-11-21 08:24:35 -0800

    .

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,11 @@ class Square:
     """0. My first square solve"""
 
     def __init__(self, size=0):
+        """__init__ method"""
         self.set_size(size)
 
     def set_size(self, size):
+        """setter method"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
