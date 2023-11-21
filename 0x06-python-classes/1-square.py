bd62be93a6354b6424972e3255141fb87545b4e6
ahmed <ahmedarafa.200444@gmail.com>
2023-11-21 08:32:12 -0800

    .

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,4 @@
 class Square:
     """0. My first square solve"""
     def __init__(self, size):
-        """__init__ method.
-
-
-           Args:
-               size (int): dfdjfsdnfs."""
         self.__size = size
