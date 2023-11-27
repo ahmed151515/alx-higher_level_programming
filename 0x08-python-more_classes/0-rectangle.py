dfc1c6fe5ff3c33fb08ae484d36538895cac71f8
ahmed <ahmedarafa.200444@gmail.com>
2023-11-27 10:31:38 -0800

    .

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""solve task 0x08. Python - More Classes and Objects"""
+
+
+class Rectangle:
+    """Rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# alx-higher_level_programming
+0x08-python-more_classes
