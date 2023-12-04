bb5c30afaad666d14187abbb1c951a966e295b50
ahmed <ahmedarafa.200444@gmail.com>
2023-12-04 09:27:27 -0800

    .

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""solve task"""
+
+def lookup(obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return dir(obj)
