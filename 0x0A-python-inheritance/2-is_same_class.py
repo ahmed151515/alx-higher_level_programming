774b24f863e8a4681466e54f6fb586b60abb789e
ahmed <ahmedarafa.200444@gmail.com>
2023-12-04 09:39:28 -0800

    .

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return type(obj) == a_class
