3fe0f1dd14ae95d47a750c04e688f4351718ceaf
ahmed <ahmedarafa.200444@gmail.com>
2023-12-04 10:12:45 -0800

    .

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+class BaseGeometry:
+    """_summary_
+    """    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+
+        Returns:
+            _type_: _description_
+        """        
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be greater than 0")
+        return value
+
+class Rectangle(BaseGeometry):
+    """_summary_
+
+    Args:
+        BaseGeometry (_type_): _description_
+    """    
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
+    def area(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """        
+        return self.__height * self.__width
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
