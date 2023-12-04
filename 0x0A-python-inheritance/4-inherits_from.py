2046b2c06cd49900ed2c663632692f8c6535cff5
ahmed <ahmedarafa.200444@gmail.com>
2023-12-04 09:48:36 -0800

    .

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+def inherits_from(obj, a_class):
+    """_summary_
+
+    Args:
+    obj (_type_): _description_
+    a_class (_type_): _description_
+
+    Returns:
+    _type_: _description_
+    """
+    return issubclass(obj, a_class)
