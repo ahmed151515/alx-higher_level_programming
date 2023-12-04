c9f3d501b54c8b82709460fb52dd88de130545e2
ahmed <ahmedarafa.200444@gmail.com>
2023-12-04 10:24:35 -0800

    .

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+class BaseGeometry:
+    """_summary_
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be greater than 0")
+        return value
+
+
+class Rectangle(BaseGeometry):
+    """_summary_
+
+    Args:
+        BaseGeometry (_type_): _description_
+    """
+
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
+    def area(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+
+    def __init__(self, size):
+        self.__size = super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return super().area()
+
+    def __str__(self):
+        return super().__str__()
