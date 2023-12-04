14e44c2c7b4233a041d8fef938f410de408f0804
ahmed <ahmedarafa.200444@gmail.com>
2023-12-04 10:02:49 -0800

    .

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """solve task"""
 
 
@@ -6,10 +7,24 @@ class BaseGeometry:
     """_summary_
     """
     def area(self):
-        raise Exception ("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+
+        Returns:
+            _type_: _description_
+        """
         if type(value) != int:
             raise TypeError(f"{name} must be an integer")
         if value < 0:
             raise ValueError(f"{name} must be greater than 0")
+        return value
