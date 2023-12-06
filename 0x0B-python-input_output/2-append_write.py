c9094977f6910c66d89064cb6cb9cd68f8ce66f0
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-12-06 20:03:31 +0200

    .

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+def append_write(filename="", text=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+    """
+
+    with open(filename, "a", encoding="UTF-8") as f:
+
+        num = f.write(text)
