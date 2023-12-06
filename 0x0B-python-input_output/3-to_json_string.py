2b946beab29e3be1e3407182216bcd15d58579f0
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-12-06 20:10:20 +0200

    .

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,5 +6,12 @@ import json
 
 
 def to_json_string(my_obj):
+    """_summary_
 
+    Args:
+        my_obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return json.dumps(my_obj)
