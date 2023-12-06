d9d988f6a52b62e19b48c26071eb55d2dd15ba71
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-12-06 20:11:43 +0200

    .

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """_summary_
+
+    Args:
+        my_str (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return json.loads(my_str)
