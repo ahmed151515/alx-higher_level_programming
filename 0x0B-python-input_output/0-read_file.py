92368b27750483da68bd8d5bd08708e0c349b8f3
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-12-06 19:28:48 +0200

    .

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""solve task"""
+
+
+def read_file(filename=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
+    with open(filename,encoding="UTF-8") as file:
+        print(file.read())
