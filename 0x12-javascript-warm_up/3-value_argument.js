8f6cccac47322c9d4b9c6e9339dfc098130b2867
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-15 22:47:36 +0200

    .

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length[2] === undefined) {
-  console.log('No argument');
-} else {
+if (process.argv[2] !== undefined) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
