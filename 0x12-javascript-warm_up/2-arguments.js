b2c8e587073044866457491e10f0d455c2478fd7
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-15 22:29:34 +0200

    .

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (Process.argv.length <= 2) {
-    console.log('No argument');
+if (Process.argv.length < 2) {
+  console.log('No argument');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
