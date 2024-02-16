deb22da82c096511413f5f623e5c838eb699087d
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-02-16 15:54:55 +0200

    .

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+let max = parseInt(process.argv[2]);
+let secondMax = 0;
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  for (let i = 3; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > max) {
+      secondMax = max;
+      max = process.argv[i];
+    } else if (parseInt(process.argv[i]) > secondMax) {
+      secondMax = parseInt(process.argv[i]);
+    }
+
+    console.log(process.argv[i] + ' > ' + max + ' | ' + secondMax);
+  }
+  console.log(secondMax);
+}
