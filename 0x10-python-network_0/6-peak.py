7a54e4dc4184d479c85cb8fb747eade0ffc0f7da
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-03-02 16:28:45 +0200

    .

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""Find a peak in a list of unsorted integers"""
 
+
+def find_peak(list_of_integers:list):
+    list_of_integers.sort()
+    
+    if len(list_of_integers) == 0:
+        return None
+    else:
+        return list_of_integers[-1]
