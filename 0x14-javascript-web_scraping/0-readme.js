4ba7f0d3b0bd1301bbfba1553dde5f6188d71cdd
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-04-25 08:30:24 +0200

    .

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// solve task
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
