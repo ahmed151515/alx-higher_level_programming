0d6474ce7ddea66113208feb6723d1a3e36337f9
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-05-10 14:24:40 +0300

    .

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from sys import argv
+
+if __name__ == "__main__":
+
+    engine = db.create_engine(
+        "mysql://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+    session = sessionmaker(bind=engine)()
+    res = session.query(State).order_by(
+        State.id.asc()).filter(State.name.like("%a%")).all()
+    for r in res:
+        print(f"{r.id}: {r.name}")
